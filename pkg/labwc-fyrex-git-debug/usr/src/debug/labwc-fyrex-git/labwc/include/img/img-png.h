e287cb2966ee3d9f010a52d1236309c5e1180792
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2025-12-19 15:58:14 -0600

    Delete pkg directory made by PKGBUILD

--- pkg/labwc-fyrex-git-debug/usr/src/debug/labwc-fyrex-git/labwc/include/img/img-png.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-only */
-#ifndef LABWC_IMG_PNG_H
-#define LABWC_IMG_PNG_H
-
-struct lab_data_buffer *img_png_load(const char *filename);
-
-#endif /* LABWC_IMG_PNG_H */
